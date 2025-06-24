8f741e0ce3e6d0b2a37ccc1eff44af1c167390aa
Gurmeher Kathuria <gurmehersk@g.ucla.edu>
2025-06-24 12:14:54 -0700

    Update with corrections on graph

--- UpdatedDataVisualization.py
+++ UpdatedDataVisualization.py
@@ -22,8 +22,8 @@ for lcpath in lcpaths:
     pdcsap_flux = data['PDCSAP_FLUX']
     qual = data['QUALITY']
     bkgd = data['SAP_BKG'] # TODO : PLOT ME!
-    savpath = f"/home/gurmeher/gurmeher/lightcurves/sector_{sector_number}/fluxes/TIC_{tic_id}.pdf"
-    savpath2 = f"/home/gurmeher/gurmeher/lightcurves/sector_{sector_number}/bkgd/TIC_{tic_id}.pdf"
+    savpath = f"/home/gurmeher/gurmeher/lightcurves/sector_{sector_number}/TIC_{tic_id}.pdf"
+    # savpath2 = f"/home/gurmeher/gurmeher/lightcurves/sector_{sector_number}/bkgd/TIC_{tic_id}.pdf"
 
     if os.path.exists(savpath):
             print(f"Skipping TIC {tic_id} — already cached.")
@@ -35,31 +35,31 @@ for lcpath in lcpaths:
     pdcsap_flux = pdcsap_flux[sel]
     bkgd = bkgd[sel]
 
-    fig, axs = plt.subplots(nrows=2, figsize=(12,3))
-    axs[0].scatter(time, sap_flux/np.nanmedian(sap_flux), c='k', s=0.5)
-    axs[1].scatter(time, pdcsap_flux/np.nanmedian(pdcsap_flux), c='k', s=0.5)
-    
+    q1 = np.nanpercentile(sap_flux, 25)
+    q3 = np.nanpercentile(sap_flux, 75)
+    iqr = q3 - q1
+
+    # Define upper limit to clip flares
+    upper_bound = q3 + 1.5 * iqr
+    mask = sap_flux < upper_bound
+    fig, axs = plt.subplots(nrows=3, figsize=(12,3), sharex = True)
+    plt.subplots_adjust(hspace=0.3)
+    axs[0].scatter(time[mask], sap_flux[mask]/np.nanmedian(sap_flux[mask]), c='k', s=0.5, label = 'SAP')
+    axs[0].set_ylabel("Normalized SAP")
+    axs[1].scatter(time[mask], pdcsap_flux[mask]/np.nanmedian(pdcsap_flux[mask]), c='k', s=0.5, label = 'PDCSAP')
+    axs[1].set_ylabel("Normalized PDCSAP")
+    axs[2].scatter(time[mask], bkgd[mask]/np.nanmedian(bkgd[mask]), c= 'k', s = 0.5, label = 'Background')
+    axs[2].set_ylabel("Normalized Background")
+    axs[2].set_xlabel('Time (BTJD)')
 # added new folder inside each sector which has "fluxes" and "bkgd" so I can separately save them
 # keeping the sap cutoff for the pdc_sap cutoff since they should be about the same when considering flares, wouldn't change much i believe
     for ax in axs:
         
-        q1 = np.nanpercentile(sap_flux, 25)
-        q3 = np.nanpercentile(sap_flux, 75)
-        iqr = q3 - q1
-
-        # Define upper limit to clip flares
-        upper_bound = q3 + 1.5 * iqr
-
+       # initially had put the q1, q3 and everything here, caused such problems
         ax.set_xlim(time.min()-2, time.max())
-        ax.set_ylim([0, upper_bound])
+        #ax.set_ylim([0, upper_bound])
 
-    fig2, axs2 = plt.subplots(nrows = 1, figsize = (12,3))
-    axs2.scatter(time, bkgd/np.nanmedian(bkgd), c = 'k', s = 0.5)
 
     fig.savefig(savpath, bbox_inches='tight', format = "pdf")
-    fig2.savefig(savpath2, format = "pdf")
     plt.close(fig)
-    plt.close(fig2)
-    
-
    # assert 0 # remove this when not testing and actually running code
