9b0e0af9bdf4397e5d0e1b51fa60b1cd379527c5
gurmehersk <gurmehersk@g.ucla.edu>
2025-11-14 23:27:53 -0800

    ipdated using wotan

--- ScoCenPlanets/ScoCenPlanets/transit_mask.py
+++ ScoCenPlanets/ScoCenPlanets/transit_mask.py
@@ -15,16 +15,41 @@ import notch_binned_module as nb
 import numpy as np
 import logging 
 import matplotlib.pyplot as plt
+from wotan import flatten
+from transitleastsquares import transit_mask 
+import sys 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def transit_mask():
+def transit_mask(time, flux, period, duration, T0):
+    '''
+    Function to run transit_mask on given time and flux data.
+
+    Parameters
+    ----------
+    time : array-like
+        Time data of the lightcurve
+    
+    flux : array-like
+        Flux data of the lightcurve
+
+    Returns
+    -------
+    Time and flux with transits masked :D
+
+    '''
+    intransit = transit_mask(time, period, duration, T0)
+    time_masked = time[~intransit]
+    flux_masked = flux[~intransit]
+
+    return time_masked, flux_masked
+
 
 
 
 if __name__ == "__main__":
-    transit_mask()    # Load lightcurve
+
     tic_id = 88297141
 
     # Load the lightcurve
@@ -48,28 +73,66 @@ if __name__ == "__main__":
     if len(time) == len(normalized_flux):
         logger.info("Data loaded successfully with {} points.".format(len(time)))
     else:
-        logger.warning("Data length mismatch after masking.")
-        return
+        logger.error("Data length mismatch after masking, exiting....")
+        sys.exit(1)
     
     prot = nb.Lombscargle(time, normalized_flux)
 
-    if prot > 2:
-        dictionary = {"window_length" : 1}
+    wdwle = 15
+    window_length = (wdwle/100.0)*prot
 
-    else :
-        dictionary = {"window_length" : 0.5}
+    flatten_lc2, trend_lc2 = flatten(time, pdcsap_flux/np.nanmedian(pdcsap_flux), window_length = window_length, return_trend = True, method = 'biweight')
+    pdc_time_clean, flatten_lc2_clean = mp.clean_arrays(time, flatten_lc2)
 
-    clipped_flux = nb.slide_clip(
-            time, normalized_flux, window_length=dictionary['window_length'],
-            low=100, high=2, method='mad', center='median'
-        )
+    real_binned_time, real_flux_binned = nb.bin_lightcurve(time, normalized_flux)
 
-    sel = np.isfinite(time) & np.isfinite(clipped_flux)
-    pdc_time_binned = time[sel]
-    pdc_flux_binned = 1.* clipped_flux[sel]
-    assert len(time) == len(normalized_flux)
+    period = 4.64423 # found from mcmc fit
+    t0 = 3803.24126 # found from mcmc fit
+    duration = 0.14583 # 3.5 hours, in days
+    transit_model = "tls"
 
-    mosaic = (
+    time_masked, flux_masked = transit_mask(pdc_time_clean, flatten_lc2_clean, period, duration, t0)
+
+    try:
+        model2 = tls(time_masked, flux_masked)
+        min_period = 0.5  # days, or a bit more than your cadence
+        max_period = (time_masked.max() - time_masked.min()) / 2
+    
+    except Exception as e:
+        logger.error("Error initializing TLS model: {}".format(e))
+        sys.exit(1)
+
+    if transit_model == "tls":
+        results2 = model2.power(period_min = min_period, period_max = max_period)
+
+        tls_period = results2.period
+        tls_t0 = results2.T0
+
+        sde = results2.SDE
+
+        logger.info(f"TLS detected period: {tls_period} days with SDE: {sde}")
+        logger.info(f"TLS detected T0: {tls_t0} days")
+
+        epochs = np.arange(-1000, 1000)
+        transit_times = tls_t0 + (tls_period * epochs)
+        # Compute a y-position slightly below the light curve's minimum flux
+        
+        y_marker = np.nanmin(flatten_lc2) - 0.005  # or adjust the offset
+
+
+        in_transit = (transit_times > time_masked.min()) & (transit_times < time_masked.max())
+        visible_transits = transit_times[in_transit]
+        ticker = True
+        harmonic = 0
+        possible_harmonics = [0.25, 0.5, 1, 2, 3, 4]
+        for h in possible_harmonics:
+            harmonic_checker = ((np.abs(prot-(h*tls_period)))/prot)*100
+            if harmonic_checker <= 1:
+                ticker = False
+                harmonic = h
+                break
+
+        mosaic = (
             """
             AAAAAAAAAAAAAAAAA
             AAAAAAAAAAAAAAAAA
@@ -80,34 +143,36 @@ if __name__ == "__main__":
             CCCCCCCCCCCCCCCCC
             CCCCCCCCCCCCCCCCC
             CCCCCCCCCCCCCCCCC
-            DDDDDDDDDDDDDDDDD
-            DDDDDDDDDDDDDDDDD
-            DDDDDDDDDDDDDDDDD
-            EEEEEEEEEEEEEEEEE
-            EEEEEEEEEEEEEEEEE
-            EEEEEEEEEEEEEEEEE
-            FFFFGGGHHHIIIJJJJ
-            FFFFGGGHHHIIIJJJJ
             """)
-    
-    subplotter, plaxes = plt.subplot_mosaic(mosaic, figsize=(19, 14 ))
+        
+        subplotter, plaxes = plt.subplot_mosaic(mosaic, figsize=(19, 14 ))
 
-    real_binned_time, real_flux_binned = nb.bin_lightcurve(time, normalized_flux)
+        plt.subplots_adjust(hspace=0.3)
 
-    plt.subplots_adjust(hspace=0.3)
-    plaxes['A'].set_title(f"DETRENDING TIC {tic_id}")
-    plaxes['A'].scatter(real_binned_time, real_flux_binned, zorder =2,  s = 5, color = 'black')
-    plaxes['A'].set_xticks([])
+        plaxes['A'].set_title(f"DETRENDING TIC {tic_id}")
+        plaxes['A'].scatter(real_binned_time, real_flux_binned, zorder =2,  s = 5, color = 'black')
+        plaxes['A'].set_xticks([])
+
+        plaxes['A'].plot(time, trend_lc2, linewidth = 2, zorder = 1, color = 'red')
+
+        time_flatten2_binned, flatten_lc2_binned = mp.bin_lightcurve(time, flatten_lc2)
+
+        plaxes['B'].scatter(time_flatten2_binned, flatten_lc2_binned, s = 1.5, color = 'black')
+
+        for t in visible_transits:
+            plaxes['A'].scatter(t, y_marker, marker='^', color='blue', s=20, zorder=3, label='Transit time' if t==visible_transits[0] else "")
+
+        period2 = results2.period
+        sde2 = results2.SDE
+
+        periods = results2.periods
+        power = results2.power
+
+        plaxes['C'].scatter(results2.folded_phase, results2.folded_y, marker = 'o', zorder =1, s = 0.25, color = 'black')
+        plaxes['C'].plot(results2.model_folded_phase, results2.model_folded_model, zorder =3, linewidth = 1, color = 'red')
 
 
-    flat_flux, trend_flux, notch = nb._run_notch(time, normalized_flux, dictionary)
-    time_flat, flat_flux_binned = nb.bin_lightcurve(time, flat_flux, bin_minutes=30)
-
-    plaxes['B'].scatter(time_flat , flat_flux_binned, s = 0.5)
-    plaxes['A'].plot(time, trend_flux, color = 'red', linewidth = 1.5, zorder = 1)
-
-    transit_model = "tls"
-
-    if transit_model == "tls":
-
-
+        outpath = "/home/gurmeher/gurmeher/Sco-Cen-Planets/ScoCenPlanets/ScoCenPlanets/transit_mask.pdf"
+        subplotter.tight_layout()
+        subplotter.savefig(outpath, bbox_inches='tight', format='pdf')
+        plt.close(subplotter)
